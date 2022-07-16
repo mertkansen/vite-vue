1c6a9796fd185aa4a05210bbd2544565289922e6
Mertkan <mertkansen@gmail.com>
2022-07-16 14:54:00 +0300

    Add styles folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import "./style.css";
+import "./styles/style.css";
 import App from "./App.vue";
 
 // Create a pinia instance (the root store) and pass it to the app as a plugin:
