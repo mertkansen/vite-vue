70ae578c2763807d0df18b482e0bfee6857d19f7
Mertkan <mertkansen@gmail.com>
2022-07-16 15:42:42 +0300

    Add user comp

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
+import User from "../pages/User.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/about", component: About },
+  { path: "/user/:id", component: User },
 ];
 
 export const router = createRouter({
