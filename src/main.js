9f496cea371fc53ef49e63598bbf996e04dd624c
Mertkan <mertkansen@gmail.com>
2022-07-16 15:37:39 +0300

    Add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { router } from "./routes/routes";
 
 import "./styles/style.css";
 import App from "./App.vue";
@@ -8,6 +9,7 @@ import App from "./App.vue";
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(router);
 app.use(pinia);
 
 app.mount("#app");
---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import Home from "../pages/Home.vue";
+import About from "../pages/About.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
